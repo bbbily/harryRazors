38fb008f8a952654cd70f2631411f301dc0a6d21
Peng Wu <peng116233402@gmail.com>
2017-03-10 13:10:32 -0700

    ui-router is working

---
+++ public/directives/nav/nav.js
@@ -0,0 +1,9 @@
+angular.module("main").directive("navDir", function() {
+  return {
+    restrict: "E",
+    templateUrl: "directives/nav/nav.html",
+    controller: function() {
+
+    }
+  }
+})
--- public/main.js
+++ public/main.js
@@ -3,10 +3,30 @@ angular.module("main", ["ui.router"])
     $stateProvider
       .state("signIn", {
         url: "/signIn",
-        templateUrl: "templates/signIn.html"
+        templateUrl: "views/signIn/signIn.html"
       })
       .state("home", {
         url: "/",
-        templateUrl: "templates/home.html"
+        templateUrl: "views/home/home.html"
+      })
+      .state("products", {
+        url: "products",
+        templateUrl: "views/products/products.html"
+      })
+      .state("shavePlans", {
+        url: "shavePlans",
+        templateUrl: "views/shavePlans/shavePlans.html"
+      })
+      .state("ourStory", {
+        url: "ourStory",
+        templateUrl: "views/ourStory/ourStory.html"
+      })
+      .state("help", {
+        url: "help",
+        templateUrl: "views/help/help.html"
+      })
+      .state("profile", {
+        url: "profile",
+        templateUrl: "views/profile/profile.html"
       })
   })
