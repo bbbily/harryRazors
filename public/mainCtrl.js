067c2ce87515be2918c78f7b7c3b41cb20b9cd68
Peng Wu <peng116233402@gmail.com>
2017-03-23 17:28:35 -0600

    fixed autoscroll for uiview

--- public/mainCtrl.js
+++ public/mainCtrl.js
@@ -1,4 +1,7 @@
-angular.module("main").controller("mainCtrl", function($scope, productsSvc) {
+angular.module("main").controller("mainCtrl", function($scope, productsSvc, $rootScope) {
+  $rootScope.$on('$stateChangeSuccess', function() {
+   document.body.scrollTop = document.documentElement.scrollTop = 0;
+});
   productsSvc.goMe().then(function(result) {
     if (result)
       productsSvc.setUserId(result.user_id)
